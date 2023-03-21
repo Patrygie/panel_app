9e0d60c0f869af2a5d0467220505731b62500ae9
Patrygie <giepert.patryk@gmail.com>
2023-03-21 15:14:02 +0100

    Project settings

---
+++ mcs_screen_app/STM32CubeIDE/Debug/Drivers/Components/ft5336.d
@@ -0,0 +1,6 @@
+Drivers/Components/ft5336.o: \
+ C:/Users/patry/Desktop/testowo\ app/mcs_screen_app/Drivers/BSP/Components/ft5336/ft5336.c \
+ C:/Users/patry/Desktop/testowo\ app/mcs_screen_app/Drivers/BSP/Components/ft5336/ft5336.h \
+ c:\users\patry\desktop\testowo\ app\mcs_screen_app\drivers\bsp\components\common\ts.h
+C:/Users/patry/Desktop/testowo\ app/mcs_screen_app/Drivers/BSP/Components/ft5336/ft5336.h:
+c:\users\patry\desktop\testowo\ app\mcs_screen_app\drivers\bsp\components\common\ts.h:
